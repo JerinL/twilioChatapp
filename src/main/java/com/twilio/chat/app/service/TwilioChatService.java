27103092fbedd9b672433c3cedfe07716a04d71e
Jerin <jerin.l@aryanow.com>
2023-11-27 15:06:56 +0530

    commit

--- src/main/java/com/twilio/chat/app/service/TwilioChatService.java
+++ src/main/java/com/twilio/chat/app/service/TwilioChatService.java
@@ -15,10 +15,10 @@ import java.util.UUID;
 public class TwilioChatService implements TwilioService {
 
 //    @Value("${app.twillio.accountSID}")
-    private String ACCOUNT_SID = "AC12b25747e694ea2409b393eea976b584";
+    private String ACCOUNT_SID = "my sid";
 
 //    @Value("${app.twillio.authToken}")
-    private String AUTH_TOKEN = "1ca56cd8ae69646de20f37a6b886799a";
+    private String AUTH_TOKEN = "my token";
     @Override
     public void sendSms(String to, String from, String body) {
         //	Twilio.init(ACCOUNT_SID, AUTH_TOKEN);
